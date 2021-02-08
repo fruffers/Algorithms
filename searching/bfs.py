0ec5e6b90d869560743ad13e01a328df35f57dd6
Louise Bilous <bywater30@gmail.com>
2021-02-08 22:56:04 +0000

    bfs

---
+++ searching/bfs.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+graph = defaultdict(list)
+
+def bfs(start):
+    visited = [False] * (max(self.graph) + 1)
+    queue = []
+
+    # queue start node and visit
+    queue.append(start)
+    visited[start] = True
+
+    # keep traversing while nodes get enqued
+    while queue:
+        # pop the first node
+        start = queue.pop()
+        print(start)
+        # visit all adjacent nodes and enqueue them
+        for i in graph[s]:
+            if visited[i] == False:
+                queue.append(i)
+                visited[i] = True
